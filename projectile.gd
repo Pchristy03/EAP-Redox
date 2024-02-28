4b19376d359c33a28bf685052a0e6c9241332371
Mvargas64 <mvargasbulnes@yahoo.com>
2024-02-28 14:30:32 -0600

    projectiles cooldown added

--- projectile.gd
+++ projectile.gd
@@ -4,8 +4,10 @@ const speed = 750
 
 var velocity_dir
 
+signal hit_asteroid
+
 func _ready():
-	velocity_dir = Vector2(position.x, position.y - 100) - position
+	velocity_dir = Vector2(0, -1)  # Adjust this direction according to your projectile's initial movement
 	velocity_dir = velocity_dir.normalized()
 
 func _physics_process(delta):
@@ -13,3 +15,8 @@ func _physics_process(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Asteroids"):
+		emit_signal("hit_asteroid", self)
+		queue_free()
