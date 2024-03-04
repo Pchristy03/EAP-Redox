039bc15a4aac261c76289bc5355cb46c972a6a04
gabelatham <78877534+gabelatham@users.noreply.github.com>
2024-03-04 17:20:23 -0600

    Fixed Accidental Merge Mistake

--- asteroid.gd
+++ asteroid.gd
@@ -1,11 +1,8 @@
-extends RigidBody2D
-
+extends CharacterBody2D
 
 @export var asteroid: PackedScene
 
-
-var velocity = Vector2.ZERO
-var velocity_dir = Vector2.ZERO
+var dest_dir = Vector2.ZERO
 
 # Define a constant speed for the asteroid
 const speed = 200
@@ -14,18 +11,23 @@ const speed = 200
 var explosion_occurred = false
 
 func _ready():
-	
+	pass
+
+func init(dest):
+	dest_dir = position.direction_to(Vector2(position.x, position.y + 1))
+	velocity += dest_dir * speed
 
 # Move the asteroid in the physics process
-func _physics_process(_delta):
+func _physics_process(delta):
 	# If explosion hasn't occurred, move the asteroid
 	if not explosion_occurred:
-		linear_velocity = velocity_dir * speed
+		velocity += position.direction_to(Vector2(390, 650)) * speed * delta
 	else:
 		# If explosion has occurred, stop the asteroid
-		linear_velocity = Vector2.ZERO
+		velocity = Vector2.ZERO
 		# Make the asteroid invisible
 		$AnimatedSprite2D.visible = false
+	move_and_slide()
 	
 # Function to handle when the asteroid exits the screen
 func _on_visible_on_screen_notifier_2d_screen_exited():
