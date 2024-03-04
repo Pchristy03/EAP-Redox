d93807898f957e8c3917c44b95ef2b15ec16340f
Mario Vargas <113541977+Mvargas64@users.noreply.github.com>
2024-03-03 22:49:21 -0600

    added particles effect for the asteroid explosions

--- asteroid.gd
+++ asteroid.gd
@@ -1,4 +1,5 @@
 extends RigidBody2D
+@export var asteroid: PackedScene
 
 var velocity = Vector2.ZERO
 var velocity_dir = Vector2.ZERO
@@ -8,9 +9,16 @@ const speed = 200
 func _ready():
 	velocity_dir = Vector2(position.x, position.y + 100) - position
 	velocity_dir = velocity_dir.normalized()
+	
 
 func _physics_process(_delta):
 	linear_velocity = velocity_dir * speed
-
+	
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("projectiles"):
+		body.queue_free()
+		body.game_over() 
