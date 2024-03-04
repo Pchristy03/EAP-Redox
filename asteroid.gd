0ab81feb930fa2096b1a9cad890df81bbe9c4cda
gabelatham <78877534+gabelatham@users.noreply.github.com>
2024-03-04 14:53:29 -0600

    Revamped Asteroid Physics

--- asteroid.gd
+++ asteroid.gd
@@ -1,18 +1,17 @@
-extends RigidBody2D
+extends CharacterBody2D
 @export var asteroid: PackedScene
 
-var velocity = Vector2.ZERO
-var velocity_dir = Vector2.ZERO
+var dest_dir = Vector2.ZERO
 
 const speed = 200
 
-func _ready():
-	velocity_dir = Vector2(position.x, position.y + 100) - position
-	velocity_dir = velocity_dir.normalized()
-	
+func init(dest):
+	dest_dir = position.direction_to(Vector2(position.x, position.y + 1))
+	velocity += dest_dir * speed
 
-func _physics_process(_delta):
-	linear_velocity = velocity_dir * speed
+func _physics_process(delta):
+	velocity += position.direction_to(Vector2(390, 650)) * speed * delta
+	move_and_slide()
 	
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
